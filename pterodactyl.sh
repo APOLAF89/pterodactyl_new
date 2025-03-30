570ddc1895f54fd6454d1858e932cbe99c4f2714
APOLAF89 <121321116+APOLAF89@users.noreply.github.com>
2025-03-30 16:38:22 -0300

    Update pterodactyl.sh

--- pterodactyl.sh
+++ pterodactyl.sh
@@ -23,7 +23,7 @@ user_password=""
 email_regex="^(([A-Za-z0-9]+((\.|\-|\_|\+)?[A-Za-z0-9]?)*[A-Za-z0-9]+)|[A-Za-z0-9]+)@(([A-Za-z0-9]+)+((\.|\-|\_)?([A-Za-z0-9]+)+)*)+\.([A-Za-z]{2,})+$"
 
 installPhpMyAdmin() {
-    cd $1
+    cd
 
     mkdir pma
     cd pma
